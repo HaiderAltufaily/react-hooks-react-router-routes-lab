24070d9699f34c791deabee950cc3a5cff183b50
haider <haiderlek@gmail.com>
2021-08-09 01:48:13 +0300

    commit tree d66511b6737b344c3d58bb17fb058393322e1484

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { actors } from "../data";
+import { v4 as uuidv4 } from "uuid";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, i) => {
+        return (
+          <div key={uuidv4()}>
+            <h2>Name: {actor.name}</h2>
+            <ul>
+              {actor.movies.map((mov, index) => {
+                return <li key={uuidv4()}> {mov} </li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { movies } from "../data";
-
+import { v4 as uuidv4 } from "uuid";
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      {" "}
+      <h1>Movies Page</h1>
+      {movies.map((movie, i) => {
+        return (
+          <div key={uuidv4()}>
+            <h2> Name: {movie.title} </h2>
+            <h4>Time: {movie.time}</h4>
+            <ul>
+              {movie.genres.map((genre, i) => {
+                return <li key={uuidv4()}> {genre} </li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
